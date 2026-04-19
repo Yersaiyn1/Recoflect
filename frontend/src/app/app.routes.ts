6d3ded1af39435311ab76aa37b38825b9269d9ae
Yersaiyn <ersajyntolegen127@gmail.com>
2026-04-19 22:00:22 +0500

    record

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import {Home} from './components/home/home';
 import {AboutUs} from './components/about-us/about-us';
+import { Records } from './pages/records/records';
+import { Goals } from './pages/goals/goals';
 import {Contacts} from './components/contacts/contacts';
 import {Login} from './pages/login/login';
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home},
   { path: 'about', component: AboutUs},
+  { path: 'records', component: Records },
+  { path: 'goals', component: Goals },
   { path: 'contacts', component: Contacts},
   { path: 'login', component: Login},
-];
+  { path: 'register', component: Register },
+];  
